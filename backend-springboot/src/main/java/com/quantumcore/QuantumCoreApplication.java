62e6a21ccaac0d21e494fe5a21fabce243ea15b7
Pasinduteshan7 <eg245365@engug.ruh.ac.lk>
2026-09-08 14:10:24 +0530

    chore(backend): initialize Spring Boot 3.4 project configuration and Maven wrapper

---
+++ backend-springboot/src/main/java/com/quantumcore/QuantumCoreApplication.java
@@ -0,0 +1,12 @@
+package com.quantumcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantumCoreApplication.class, args);
+    }
+}
